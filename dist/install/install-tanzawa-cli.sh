ec9d3de18134581af11ee0c335bfcecb0eb27691
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2023-12-19 18:40:24 +0900

    build: reorganize gradle project structure as hierarchical with multiple tools

--- dist/install/install-tanzawa-cli.sh
+++ dist/install/install-tanzawa-cli.sh
@@ -6,9 +6,9 @@ cd ${TG_TSUBAKURO_DIR}
 ./gradlew clean PublishMavenJavaPublicationToMavenLocal -PskipBuildNative
 
 cd ${TG_TANZAWA_DIR}
-./gradlew clean tanzawa-cli:assemble -PmavenLocal
+./gradlew clean tgsql:cli:assemble -PmavenLocal
 
 cd ${TG_INSTALL_DIR}
-tar xf ${TG_TANZAWA_DIR}/modules/cli/build/distributions/tanzawa-cli-shadow-*.tar --strip-components 1
+tar xf ${TG_TANZAWA_DIR}/modules/tgsql/cli/build/distributions/cli-shadow-*.tar --strip-components 1
 
 echo "$(basename $0) successful."
