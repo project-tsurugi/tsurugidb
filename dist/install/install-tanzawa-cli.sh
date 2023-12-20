3c1a8edb0d5c84653cc7e5daf710fe20f6e095ec
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2023-12-20 16:21:21 +0900

    feat(tanzawa): add tgdump command

--- dist/install/install-tanzawa-cli.sh
+++ dist/install/install-tanzawa-cli.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
 
-echo -e "\n[Install Tsurugi SQL Console]"
+echo -e "\n[Install Tanzawa CLI]"
 
 cd ${TG_TSUBAKURO_DIR}
 ./gradlew clean PublishMavenJavaPublicationToMavenLocal -PskipBuildNative
 
 cd ${TG_TANZAWA_DIR}
-./gradlew clean tgsql:cli:assemble -PmavenLocal
+./gradlew clean tgsql:cli:assemble tgdump:cli:assemble -PmavenLocal
 
 cd ${TG_INSTALL_DIR}
 tar xf ${TG_TANZAWA_DIR}/modules/tgsql/cli/build/distributions/cli-shadow-*.tar --strip-components 1
+tar xf ${TG_TANZAWA_DIR}/modules/tgdump/cli/build/distributions/cli-shadow-*.tar --strip-components 1
 
 echo "$(basename $0) successful."
