46010d3c667947caa1c600eda1f361c441c8f641
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2023-12-26 17:55:11 +0900

    dist: change distribuion archive name of tanzawa cli

--- dist/install/install-tanzawa-cli.sh
+++ dist/install/install-tanzawa-cli.sh
@@ -9,7 +9,7 @@ cd ${TG_TANZAWA_DIR}
 ./gradlew clean tgsql:cli:assemble tgdump:cli:assemble -PmavenLocal
 
 cd ${TG_INSTALL_DIR}
-tar xf ${TG_TANZAWA_DIR}/modules/tgsql/cli/build/distributions/cli-shadow-*.tar --strip-components 1
-tar xf ${TG_TANZAWA_DIR}/modules/tgdump/cli/build/distributions/cli-shadow-*.tar --strip-components 1
+tar xf ${TG_TANZAWA_DIR}/modules/tgsql/cli/build/distributions/tgsql-*-shadow.tar.gz --strip-components 1
+tar xf ${TG_TANZAWA_DIR}/modules/tgdump/cli/build/distributions/tgdump-*-shadow.tar.gz --strip-components 1
 
 echo "$(basename $0) successful."
