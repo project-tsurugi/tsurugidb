d74d92d01a314cae7330d723f6b479c09eda5a90
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2024-06-24 15:24:21 +0900

    dist: add apt-install-nativelib.sh for setup ipc-client runtime env

---
+++ dist/install/apt-install-nativelib.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update -y
+apt-get install -y -V \
+ build-essential \
+ cmake \
+ libboost-system-dev \
+ openjdk-11-jdk \
+ ninja-build
+
+echo "$(basename $0) successful."
