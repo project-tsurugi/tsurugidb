ed1bec9579d5f65d2d9821ee052d965635f3c846
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2025-03-04 22:22:09 +0900

    dist: add dnf-install.sh

---
+++ dist/install/dnf-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root" 1>&2
+  exit 1
+fi
+
+dnf update -y
+dnf install -y epel-release
+dnf config-manager --set-enabled epel crb
+
+dnf install -y \
+ bison \
+ boost-devel \
+ cmake \
+ doxygen \
+ flex \
+ gcc-c++ \
+ glog-devel \
+ icu \
+ java-11-openjdk-devel \
+ jemalloc-devel \
+ mpdecimal-devel \
+ ninja-build \
+ numactl-devel \
+ protobuf-devel \
+ rocksdb-devel \
+ tbb-devel \
+ uuid-devel
+
+dnf install -y https://apache.jfrog.io/artifactory/arrow/almalinux/"$(cut -d: -f5 /etc/system-release-cpe | cut -d. -f1)"/apache-arrow-release-latest.rpm
+dnf install -y \
+ arrow-devel-16.1.0-1.el9.x86_64 \
+ parquet-devel-16.1.0-1.el9.x86_64
+
+echo "$(basename $0) successful."
