b34da196500675548364d96fe44662192310e25d
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2025-03-05 14:33:23 +0900

    dist: improve apt install for apache arrow in apt-install.sh

--- dist/install/apt-install.sh
+++ dist/install/apt-install.sh
@@ -45,7 +45,7 @@ apt-get install -y -V libmpdec-dev || true
 curl --retry 3 --retry-all-errors -OL https://apache.jfrog.io/artifactory/arrow/"$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb"
 apt-get install -y -V ./apache-arrow-apt-source-latest-"$(lsb_release --codename --short).deb"
 apt-get update -y
-apt-get install -y -V libparquet-dev=16.1.0-1 libparquet-glib-dev=16.1.0-1 libarrow-dev=16.1.0-1 libarrow-glib-dev=16.1.0-1 libarrow-acero-dev=16.1.0-1 gir1.2-parquet-1.0=16.1.0-1 gir1.2-arrow-1.0=16.1.0-1
+apt-get install -y -V libparquet-dev=16.1.0-1 libparquet1600=16.1.0-1 libarrow-dev=16.1.0-1 libarrow1600=16.1.0-1
 rm -f ./apache-arrow-apt-source-latest-"$(lsb_release --codename --short).deb"
 
 echo "$(basename $0) successful."
